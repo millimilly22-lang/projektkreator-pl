f994b098686748318b336705d4c7b7e1ef501bb4
millimilly22-lang <millimilly22@gmail.com>
2026-09-13 20:48:50 +0200

    Replace ProjektKreator.pl with approved HTML design

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from'react';
-import{createRoot}from'react-dom/client';
-import ExactApp from'./exact-app.jsx';
-import'./reference-images.css';
-
-createRoot(document.getElementById('root')).render(<ExactApp/>);
