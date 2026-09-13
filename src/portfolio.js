47c9afd349f8afda74508294ceab301050e4ea68
millimilly22-lang <millimilly22@gmail.com>
2026-09-13 19:34:06 +0200

    Match Realizacje card order to supplied reference

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,5 +1,8 @@
 export const projects = [
+  { id:'latavola', name:'La Tavola', type:'Strona internetowa', url:'/portfolio/latavola/', desc:'Elegancka strona restauracji z systemem rezerwacji online.' },
+  { id:'novabud', name:'NovaBud', type:'Strona dla firmy budowlanej', url:'/portfolio/novabud/', desc:'Profesjonalna strona z prezentacją realizacji i formularzem wyceny.' },
+  { id:'greenlife', name:'GreenLife', type:'Koncepcja sklepu internetowego', desc:'Sklep z naturalnymi kosmetykami z indywidualną grafiką i blogiem.', image:'https://images.unsplash.com/photo-1596462502278-27bfdc403348?auto=format&fit=crop&w=800&q=80' },
+  { id:'clinic', name:'Klinika Harmonia', type:'Koncepcja strony internetowej', desc:'Nowoczesna strona kliniki z rejestracją online i cennikiem usług.', image:'https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?auto=format&fit=crop&w=800&q=80' },
+  { id:'fitzone', name:'FitZone', type:'Koncepcja aplikacji webowej', desc:'Platforma do treningów online z panelem użytkownika.', image:'https://images.unsplash.com/photo-1538805060514-97d9cc17730c?auto=format&fit=crop&w=800&q=80' },
   { id:'modea', name:'MODÉA', type:'Sklep internetowy', url:'/portfolio/modea/', desc:'Kolekcja mody, wyszukiwanie, filtry, podgląd produktów i działający koszyk.' },
-  { id:'latavola', name:'La Tavola', type:'Strona restauracji', url:'/portfolio/latavola/', desc:'Włoskie menu, galeria i formularz rezerwacji z wyborem terminu oraz potwierdzeniem.' },
-  { id:'novabud', name:'NovaBud', type:'Strona firmy budowlanej', url:'/portfolio/novabud/', desc:'Usługi, realizacje i formularz zapytania o wycenę z podsumowaniem.' },
 ];
